185f45daf04e1333ac739d58e93dd074f9c2df44
Maria <pavlov303@yandex.ru>
2023-09-15 22:22:56 +0300

    new fix docker-compose.yml

--- ewm-service/src/main/java/ru/practicum/ewmmain/EwmServiceApplication.java
+++ ewm-service/src/main/java/ru/practicum/ewmmain/EwmServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EwmMainApplication {
+public class EwmServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EwmMainApplication.class, args);
+        SpringApplication.run(EwmServiceApplication.class, args);
     }
 
 }
