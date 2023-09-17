1a55fac6be3f575d349f1b26b7f9847daa0f5900
Mariya <pavlov303@yandex.ru>
2023-09-17 07:58:42 +0300

    fix docker

--- stats-server/server/src/main/java/ru/practicum/server/model/ViewStats.java
+++ stats-server/server/src/main/java/ru/practicum/server/model/ViewStats.java
@@ -1,15 +1,21 @@
 package ru.practicum.server.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "view_stats")
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Data
+@Getter
+@Setter
 public class ViewStats {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String app;
     private String uri;
     private Long hits;
