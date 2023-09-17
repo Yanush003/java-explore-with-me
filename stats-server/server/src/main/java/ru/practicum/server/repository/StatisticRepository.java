3d5a7428acc2cd122f9ada23d10745df88c6112e
Maria <pavlov303@yandex.ru>
2023-09-17 18:52:08 +0300

    new fix docker-compose.yml

--- stats-server/server/src/main/java/ru/practicum/server/repository/StatisticRepository.java
+++ stats-server/server/src/main/java/ru/practicum/server/repository/StatisticRepository.java
@@ -1,35 +1,10 @@
 package ru.practicum.server.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.practicum.server.model.EndpointHit;
-import ru.practicum.server.model.ViewStats;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Repository
 public interface StatisticRepository extends JpaRepository<EndpointHit, Long> {
-
-    @Query("SELECT new ru.practicum.server.model.ViewStats(sub.app, sub.uri, COUNT(sub.ip)) " +
-            "FROM (" +
-            "    SELECT e.app as app, e.uri as uri, e.ip as ip " +
-            "    FROM EndpointHit e " +
-            "    WHERE e.dateTime BETWEEN ?1 AND ?2 " +
-            "    AND (e.uri IN (?3) OR (?3) is NULL) " +
-            ") sub " +
-            "GROUP BY sub.app, sub.uri")
-    List<ViewStats> findViewStatList(LocalDateTime start, LocalDateTime end, List<String> uris);
-
-    @Query("SELECT new ru.practicum.server.model.ViewStats(sub.app, sub.uri, COUNT(DISTINCT sub.ip)) " +
-            "FROM (" +
-            "    SELECT e.app as app, e.uri as uri, e.ip as ip " +
-            "    FROM EndpointHit e " +
-            "    WHERE e.dateTime BETWEEN ?1 AND ?2 " +
-            "    AND (e.uri IN (?3) OR (?3) is NULL) " +
-            ") sub " +
-            "GROUP BY sub.app, sub.uri")
-    List<ViewStats> findViewStatListUniqueIp(LocalDateTime start, LocalDateTime end, List<String> uris);
 
 }
